4a0e37a7243d39efceb159b9b98db8fb8f284d10
Emmanuel Janssens <emmanueljanssens72@gmail.com>
2017-10-05 08:17:34 +0200

    presentation 5/10/2017

--- Globe/js/square.js
+++ Globe/js/square.js
@@ -42,7 +42,7 @@ class Square
         var planeMaterial;
         var texture; 
 
-       /*if(this.fileExist(file))
+       if(this.fileExist(file))
         {
             texture =  this.textureLoader.load( file );      
             texture.wrapT = THREE.RepeatWrapping;
@@ -53,7 +53,11 @@ class Square
             console.log(file);     
             scene.add(this.planeMesh);
         }
-        else*/
+        else
+            {
+                this.planeMesh = null;
+            }
+   /*     else
         {
             
             planeMaterial; new THREE.MeshPhongMaterial( {  color: 0xffffff});
@@ -61,25 +65,31 @@ class Square
             this.planeMesh.position.set(this.x,this.y,this.z);
             scene.add(this.planeMesh);
             
-        }
+        }*/
     }
 
 
     setPosition(x,y,z)
     {
-        this.planeMesh.position.set(x,y,z);       
+        if(this.planeMesh != null)
+            this.planeMesh.position.set(x,y,z);       
     }
     lookAtZero()
     {
-        var v = new THREE.Vector3();
-        v.subVectors(this.planeMesh.position, new THREE.Vector3(0,0,0)).add(this.planeMesh.position);
-        this.planeMesh.lookAt(v);
+        if(this.planeMesh != null)
+        {
+            var v = new THREE.Vector3();
+            v.subVectors(this.planeMesh.position, new THREE.Vector3(0,0,0)).add(this.planeMesh.position);
+            this.planeMesh.lookAt(v);
+        }            
+
 
         
     }
     setPosSpherical(spherical)
     {
-        this.planeMesh.position.setFromSpherical(spherical);       
+        if(this.planeMesh != null)            
+            this.planeMesh.position.setFromSpherical(spherical);       
 
     }
     getPosition()
